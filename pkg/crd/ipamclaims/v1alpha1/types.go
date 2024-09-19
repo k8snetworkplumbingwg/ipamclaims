36b122ef776fffa019bc3bb8cebbb830f98f8c6a
Miguel Duarte Barroso <mdbarroso@redhat.com>
2024-09-19 15:53:52 +0200

    WIP: does this bump work in CI ?
    Signed-off-by: Miguel Duarte Barroso <mdbarroso@redhat.com>

--- pkg/crd/ipamclaims/v1alpha1/types.go
+++ pkg/crd/ipamclaims/v1alpha1/types.go
@@ -4,13 +4,13 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-//go:generate go run sigs.k8s.io/controller-tools/cmd/controller-gen@v0.16.2 paths=./... object crd output:artifacts:code=./,config=../../../../artifacts
+//go:generate go run sigs.k8s.io/controller-tools/cmd/controller-gen@v0.16.3 paths=./... object crd output:artifacts:code=./,config=../../../../artifacts
 
-//go:generate go run k8s.io/code-generator/cmd/client-gen@v0.31.0 client-gen --go-header-file ../../../../hack/custom-boilerplate.go.txt --clientset-name versioned --input-base "" --input github.com/k8snetworkplumbingwg/ipamclaims/pkg/crd/ipamclaims/v1alpha1 --output-pkg github.com/k8snetworkplumbingwg/ipamclaims/pkg/crd/ipamclaims/v1alpha1/apis/clientset --output-dir ./apis/clientset ..
+//go:generate go run k8s.io/code-generator/cmd/client-gen@v0.31.1 client-gen --go-header-file ../../../../hack/custom-boilerplate.go.txt --clientset-name versioned --input-base "" --input github.com/k8snetworkplumbingwg/ipamclaims/pkg/crd/ipamclaims/v1alpha1 --output-pkg github.com/k8snetworkplumbingwg/ipamclaims/pkg/crd/ipamclaims/v1alpha1/apis/clientset --output-dir ./apis/clientset ..
 
-//go:generate go run k8s.io/code-generator/cmd/lister-gen@v0.31.0 --go-header-file ../../../../hack/custom-boilerplate.go.txt --output-pkg github.com/k8snetworkplumbingwg/ipamclaims/pkg/crd/ipamclaims/v1alpha1/apis/listers --output-dir ./apis/listers ./
+//go:generate go run k8s.io/code-generator/cmd/lister-gen@v0.31.1 --go-header-file ../../../../hack/custom-boilerplate.go.txt --output-pkg github.com/k8snetworkplumbingwg/ipamclaims/pkg/crd/ipamclaims/v1alpha1/apis/listers --output-dir ./apis/listers ./
 
-//go:generate go run k8s.io/code-generator/cmd/informer-gen@v0.31.0 --go-header-file ../../../../hack/custom-boilerplate.go.txt --versioned-clientset-package github.com/k8snetworkplumbingwg/ipamclaims/pkg/crd/ipamclaims/v1alpha1/apis/clientset/versioned --listers-package github.com/k8snetworkplumbingwg/ipamclaims/pkg/crd/ipamclaims/v1alpha1/apis/listers --output-pkg github.com/k8snetworkplumbingwg/ipamclaims/pkg/crd/ipamclaims/v1alpha1/apis/informers  --output-dir ./apis/informers ./
+//go:generate go run k8s.io/code-generator/cmd/informer-gen@v0.31.1 --go-header-file ../../../../hack/custom-boilerplate.go.txt --versioned-clientset-package github.com/k8snetworkplumbingwg/ipamclaims/pkg/crd/ipamclaims/v1alpha1/apis/clientset/versioned --listers-package github.com/k8snetworkplumbingwg/ipamclaims/pkg/crd/ipamclaims/v1alpha1/apis/listers --output-pkg github.com/k8snetworkplumbingwg/ipamclaims/pkg/crd/ipamclaims/v1alpha1/apis/informers  --output-dir ./apis/informers ./
 
 // +genclient
 // +kubebuilder:object:root=true
